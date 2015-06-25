c22364cba7eb554f243b53ffa2a5baafb3f816c0
wltiii <bill.turner@changent.com>
2015-06-25 13:10:42 -0500

    add ampersand-app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import Router from './router'
 import styles from './styles/main.styl'
+import app from 'ampersand-app'
 
-window.app = {
+app.extend(){
   init() {
     this.router = new Router()
     this.router.history.start()
   }
-}
+})
 
-window.app.init()
+app.init()
