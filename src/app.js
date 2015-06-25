881e178b13cecb51ea2f8212fe0b7a7bba5cd1f7
wltiii <bill.turner@changent.com>
2015-06-25 10:20:07 -0500

    react and es6 start

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-document.write('Hello Frontend Masters!')
+import React from 'react'
+import styles from './styles/main.css'
+// es6 version - see appReact.js for original
+// notice the use of const versus var
+
+const Hello = React.createClass({
+  render: function() {
+    return <div>Hello, {this.props.name}</div>
+  }
+})
+
+React.render(<Hello name="Billy"/>, document.body)
